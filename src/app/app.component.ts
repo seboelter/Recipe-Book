f71f7b8e1d441194fa21a378e1e206679d4cc5a4
Sarah Boelter <sboelter@mtaonline.net>
2020-06-09 20:27:08 -0800

    Styles component with multiline CSS, how to write the component in HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+//css can be added directly to the styles tag, styles(urls) is an array that holds sheets, or multiline css
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles:[`
+    h3{
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
 }
